a0f3c056a9b2146aba2104d6c34212059d3a6d60
argenspin <argenspin@gmail.com>
2023-10-10 16:24:27 +0530

    another attempt at a solution for duplicates in child_ids and in_charge_child_ids

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -12,40 +12,40 @@ class HrEmployeeInherit(models.Model):
     is_in_charge = fields.Boolean(string="Is In charge")
     in_charge_child_ids = fields.One2many("hr.employee","in_charge_id",string="In Charge Childs")
 
-    def get_organisation_data(self,employee, processed_ids=None):
-        if processed_ids is None:
-            processed_ids = set()
+    # def get_organisation_data(self,employee, processed_ids=None):
+    #     if processed_ids is None:
+    #         processed_ids = set()
 
-        # Skip if the employee has already been processed
-        if employee.id in processed_ids:
-            return None
+    #     # Skip if the employee has already been processed
+    #     if employee.id in processed_ids:
+    #         return None
 
-        # Add the current employee to the set of processed IDs
-        processed_ids.add(employee.id)
+    #     # Add the current employee to the set of processed IDs
+    #     processed_ids.add(employee.id)
 
-        # Determine the set of children based on availability of in_charge_child_ids
-        children_ids = employee.in_charge_child_ids if employee.in_charge_child_ids else employee.child_ids
+    #     # Determine the set of children based on availability of in_charge_child_ids
+    #     children_ids = employee.in_charge_child_ids if employee.in_charge_child_ids else employee.child_ids
 
-        # Build the hierarchy for the current employee
-        org_data = {
-            'id': employee.id,
-            'name': employee.name,
-            'title': employee.job_id.name,
-            'image':employee.image_1920,
-            'children': [],
-        }
+    #     # Build the hierarchy for the current employee
+    #     org_data = {
+    #         'id': employee.id,
+    #         'name': employee.name,
+    #         'title': employee.job_id.name,
+    #         'image':employee.image_1920,
+    #         'children': [],
+    #     }
 
-        # Recursively get hierarchy for children
-        for child_id in children_ids:
-            child_hierarchy = self.get_organisation_data(child_id, processed_ids)
-            if child_hierarchy:
-                org_data['children'].append(child_hierarchy)
+    #     # Recursively get hierarchy for children
+    #     for child_id in children_ids:
+    #         child_hierarchy = self.get_organisation_data(child_id, processed_ids)
+    #         if child_hierarchy:
+    #             org_data['children'].append(child_hierarchy)
 
-        return org_data
+    #     return org_data
 
 
 
-    # def get_organisation_data(self, employee):
+    def get_organisation_data(self, employee):
     #     org_data = {'id': employee.id, 'name': employee.name, 'title': employee.job_title,'image':employee.image_1920, 'children': []}
         
     #     child_ids = False
@@ -62,55 +62,55 @@ class HrEmployeeInherit(models.Model):
     #             org_data['children'].append(self.get_organisation_data(subordinate))
 
     #     return org_data
-        # org_data = {}
-        # logger = logging.getLogger("Debugger: ")
-        # child_ids = False
-        # if employee:
-        #     already_added_ids = [employee.id]
-        #     org_data['id'] = employee.id
-        #     org_data['name'] = employee.name
-        #     org_data['title'] = employee.job_title
-        #     org_data['image'] = employee.image_1920
-        #     org_data['children'] = []
-        #     in_charges = self.env['hr.employee'].search([('in_charge_child_ids','!=',False),('parent_id','=',employee.id)])
-        #     for in_charge in in_charges:
-        #         already_added_ids.append(in_charge.id)
-        #         in_charge_data = {}
-        #         in_charge_data['id'] = in_charge.id
-        #         in_charge_data['name'] = in_charge.name
-        #         in_charge_data['title'] = in_charge.job_title
-        #         in_charge_data['image'] = in_charge.image_1920
+        org_data = {}
+        logger = logging.getLogger("Debugger: ")
+        child_ids = False
+        if employee:
+            already_added_ids = [employee.id]
+            org_data['id'] = employee.id
+            org_data['name'] = employee.name
+            org_data['title'] = employee.job_title
+            org_data['image'] = employee.image_1920
+            org_data['children'] = []
+            in_charges = self.env['hr.employee'].search([('in_charge_child_ids','!=',False),('parent_id','=',employee.id)])
+            for in_charge in in_charges:
+                already_added_ids.append(in_charge.id)
+                in_charge_data = {}
+                in_charge_data['id'] = in_charge.id
+                in_charge_data['name'] = in_charge.name
+                in_charge_data['title'] = in_charge.job_title
+                in_charge_data['image'] = in_charge.image_1920
 
-        #         in_charge_data['children'] = []
-        #         if in_charge.in_charge_child_ids:
-        #             child_ids = in_charge.in_charge_child_ids
-        #         else:
-        #             child_ids = in_charge.child_ids
-        #         for sub_ord in child_ids:
-        #             already_added_ids.append(sub_ord.id)
-        #             sub_ord_data = {}
-        #             sub_ord_data['id'] = sub_ord.id
-        #             sub_ord_data['name'] = sub_ord.name
-        #             sub_ord_data['title'] = sub_ord.job_title
-        #             sub_ord_data['image'] = sub_ord.image_1920
+                in_charge_data['children'] = []
+                if in_charge.in_charge_child_ids:
+                    child_ids = in_charge.in_charge_child_ids
+                else:
+                    child_ids = in_charge.child_ids
+                for sub_ord in child_ids:
+                    already_added_ids.append(sub_ord.id)
+                    sub_ord_data = {}
+                    sub_ord_data['id'] = sub_ord.id
+                    sub_ord_data['name'] = sub_ord.name
+                    sub_ord_data['title'] = sub_ord.job_title
+                    sub_ord_data['image'] = sub_ord.image_1920
 
-        #             in_charge_data['children'].append(sub_ord_data)
+                    in_charge_data['children'].append(sub_ord_data)
                     
-        #         org_data['children'].append(in_charge_data)
+                org_data['children'].append(in_charge_data)
 
-        #     other_subords = self.env['hr.employee'].search([('in_charge_child_ids','=',False),('id','not in',already_added_ids),('parent_id','=',employee.id)])
-        #     for sub_ord in other_subords:
-        #         sub_ord_data = {}
-        #         sub_ord_data['id'] = sub_ord.id
-        #         sub_ord_data['name'] = sub_ord.name
-        #         sub_ord_data['title'] = sub_ord.job_title
-        #         sub_ord_data['image'] = sub_ord.image_1920
+            other_subords = self.env['hr.employee'].search([('in_charge_child_ids','=',False),('id','not in',already_added_ids),('parent_id','=',employee.id)])
+            for sub_ord in other_subords:
+                sub_ord_data = {}
+                sub_ord_data['id'] = sub_ord.id
+                sub_ord_data['name'] = sub_ord.name
+                sub_ord_data['title'] = sub_ord.job_title
+                sub_ord_data['image'] = sub_ord.image_1920
 
-        #         org_data['children'].append(sub_ord_data)
+                org_data['children'].append(sub_ord_data)
 
-        #     logger.error("org_data: "+str(org_data))
+            logger.error("org_data: "+str(org_data))
                 
-        # return org_data
+        return org_data
 
 
     # def get_organisation_data(self, employee):
