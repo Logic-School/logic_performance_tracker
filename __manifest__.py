5659519d72f8466b3c5a319749f97ec7510f7b8c
argenspin <argenspin@gmail.com>
2023-10-11 18:34:23 +0530

    added css files to manifest

--- __manifest__.py
+++ __manifest__.py
@@ -11,19 +11,20 @@
         'views/employee_views.xml',
 
     ],
-    'assets':{'web.assets_qweb': [
-        'logic_performance_tracker/static/src/css/dashboard_card_view.css',
-        'logic_performance_tracker/static/src/css/lib/jquery.orgchart.css',
-        'logic_performance_tracker/static/src/css/lib/orgchart_styles.css',
-        'logic_performance_tracker/static/src/css/lib/other_styles.css',
+    # 'assets':{'web.assets_qweb': [
 
-    ],
-    },
+
+    # ],
+    # },
     'qweb': ["static/src/xml/dashboard_templates.xml"],
     'assets': {
     'web.assets_backend': [
         '/logic_performance_tracker/static/src/js/dashboard_card_view.js',
-        "/logic_performance_tracker/static/src/scss/dashboard_card_view.scss"
+        '/logic_performance_tracker/static/src/scss/dashboard_card_view.scss',
+        '/logic_performance_tracker/static/src/css/dashboard_card_view.css',
+        '/logic_performance_tracker/static/src/css/lib/jquery.orgchart.css',
+        '/logic_performance_tracker/static/src/css/lib/orgchart_styles.css',
+        '/logic_performance_tracker/static/src/css/lib/other_styles.css',
     ]},
     'demo': [],
     'summary': "Performance Tracker",
